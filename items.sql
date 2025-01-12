1df3705aaf4b544cb412e85c58ce57fe4c7df24e
tegar2612 <tegar122604@gmail.com>
2025-01-12 13:04:57 +0700

    items.sql

---
+++ items.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS WebBasic.items (
+
+  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+
+  title varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+
+  description text COLLATE utf8_unicode_ci NOT NULL,
+
+  created_at timestamp NULL DEFAULT NULL,
+
+  updated_at timestamp NULL DEFAULT NULL,
+
+  PRIMARY KEY (`id`)
+
+)  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=63 ;
